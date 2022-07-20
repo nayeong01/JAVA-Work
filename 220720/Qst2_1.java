56279a921a18d2c88135ffae6995cfda9e61dda9
nayeong01 <108383043+nayeong01@users.noreply.github.com>
2022-07-20 22:23:47 +0900

    Update Qst2_1.java

--- 220720/Qst2_1.java
+++ 220720/Qst2_1.java
@@ -1,26 +1,31 @@
+
 public class Qst2_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int monthIn = 2;
-		int days = 25;
-		int year = 2012;
+								// 외부값을 넘겨줄 수가 있음. 변수 이름을 맞춰줘야 한다.
+		//String[] arguments = new String[2];
 		
-		if (args.length >0) {
-			monthIn = Integer.parseInt(args[0]);
-		}
+		int yearIn = 1980;
+		/*
 		if (args.length > 0) {
-			days = Integer.parseInt(args[1]);
-		}
+			monthIn = Integer.parseInt(args[0]);
+		}*/
 		if (args.length > 1) {
-			year = Integer.parseInt(args[2]);
+			yearIn = Integer.parseInt(args[1]);
 		}
-		System.out.println(countDays(monthIn,days, year));
-	}
+		
+		int monthIn;
+		for (monthIn=1; monthIn<=12; monthIn++) {
+			int j;
+			for (j=1; j<=countDays(monthIn,yearIn); j++)
+			System.out.printf("%d/%d%n",monthIn,j);
+		}
 
-	private static char[] countDays(int month, int day, int year) {
-		// TODO Auto-generated method stub
-		int count = -1;
+	}
+	
+		
+	static int countDays(int month, int year) {
+			int count = -1;
 		switch(month) {
 			case 1:
 			case 3:
@@ -29,36 +34,24 @@ public class Qst2_1 {
 			case 8:
 			case 10:
 			case 12:
-				String[] days1 = new String[31];
-				int i;
-				for(i=0; i <days1.length; i++);
-					days1[i]= "month";		
-					System.out.println(days1[i]+"/"+i);
+				count =31;
 				break;
 			case 4:
 			case 6:
 			case 9:
 			case 11:
-				String[] days2 = new String[30];
-				int j;
-				for(j=0; j<days2.length; j++);
-					days2[j]="month";
-					System.out.println(days2[j]+"/"+j);
+				count = 30;
 				break;
 			case 2:
 				if (year % 4 ==0) {
-					String[] days3 = new String[29];
-					int a;
-					for(a=0; a<days3.length; a++);
-						days3[a]= "2";
-						System.out.println(days3[a] + "/" + a);	
+					count = 29;
 				} else {
 					count = 28;
 				}
 				if ((year % 100 ==0)&(year % 400 !=0)) {
 					count = 28;
+			}	
 		}
+		return count;
 	}
-	
-
 }
